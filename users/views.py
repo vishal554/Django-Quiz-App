7343c948fc433c59ae732b1a269184c8c4531c7e
Vishal Panchal <vishalpanchal1338@gmail.com>
2021-07-02 14:16:56 +0530

    Fixed 2 factor auth issue
    previously user were getting created even when the OTP is not entered.
    Now user can only be created after correct OTP is entered

--- users/views.py
+++ users/views.py
@@ -1,7 +1,4 @@
-from django.contrib.auth import forms
-from django.db.models.query_utils import Q
-from django.http import request
-from django.http.response import JsonResponse
+from django.contrib.auth.forms import UserCreationForm
 from users.forms import UserRegisterForm
 from django.shortcuts import redirect, render
 from django.contrib import messages
@@ -10,25 +7,6 @@ from django.core.mail import send_mail
 import random
 from django.views import View
 from .models import *
-import ast
-import uuid
-
-# Create your views here.
-
-# def register(request):
-#     if request.method == "POST":
-#         form = UserRegisterForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             email = form.cleaned_data.get("email")
-#             username = form.cleaned_data.get("username")
-#             request.session['email'] = email
-#             request.session['username'] = username
-#             return redirect('otp_verification')
-#     else:
-#         form = UserRegisterForm()
-
-#     return render(request,'users/register.html', {'form': form})
 
 
 class Register(View):
@@ -40,13 +18,55 @@ class Register(View):
     def post(self, request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            request.session['form_data'] = form.cleaned_data
             email = form.cleaned_data.get("email")
             username = form.cleaned_data.get("username")
             request.session['email'] = email
             request.session['username'] = username
             return redirect('otp_verification')
         return render(request,'users/register.html', {'form': form})
+
+
+class OtpVerification(View):
+    
+    def get(self, request):
+        sent = False
+        try:
+            email = request.session['email']
+        except:
+            return redirect('register')
+        if sent==False:
+            otp = random.randint(11111, 99999)
+            request.session['otp'] = otp
+            send_mail('OTP Verification', f'Your OTP is: {otp}', 'vishalpanchal338@gmail.com', [email], fail_silently=False)
+            messages.success(request, f"OTP Successfully sent to {email}. Please check your email!")
+            sent = True
+        else:
+            messages.warning(request, f'Already sent email with OTP')
+        
+        return render(request, 'users/otpverification.html')
+        
+    def post(self, request):
+        entered_otp = request.POST["otp"]
+        otp = request.session['otp']
+        if int(entered_otp) == int(otp):
+            form_data = request.session['form_data']
+
+            form = UserCreationForm(form_data)
+            if form.is_valid():
+                print("Valid Form")
+                User.objects.create(username=form_data['username'], password=form_data['password1'], email=form_data['email'])
+                username = request.session['username']
+                messages.success(request, f"Account successfully created for {username}! You can login Now")
+                return redirect('login') 
+            else:
+                print("not valid form")
+                messages.warning(request, f"Invalid username or password. Please Register again")
+                return redirect('register') 
+            
+        else:
+            messages.error(request, "Invalid OTP! Please try again")
+        return render(request, 'users/otpverification.html')
 
 
 class Home(View):
@@ -106,35 +126,6 @@ class TakeQuiz(View):
         return render(request, 'users/profile.html')
 
 
-class OtpVerification(View):
-    
-    def get(self, request):
-        sent = False
-        try:
-            email = request.session['email']
-        except:
-            return redirect('register')
-        if sent==False:
-            otp = random.randint(11111, 99999)
-            request.session['otp'] = otp
-            send_mail('OTP Verification', f'Your OTP is: {otp}', 'vishalpanchal338@gmail.com', [email], fail_silently=False)
-            messages.success(request, f"OTP Successfully sent to {email}. Please check your email!")
-            sent = True
-        else:
-            messages.warning(request, f'Already sent email with OTP')
-        return render(request, 'users/otpverification.html')
-        
-    def post(self, request):
-        entered_otp = request.POST["otp"]
-        otp = request.session['otp']
-        if int(entered_otp) == int(otp):
-            #form = request.session['form']
-            username = request.session['username']
-            messages.success(request, f"Account successfully created for {username}! You can login Now")
-            return redirect('login') 
-        else:
-            messages.error(request, "Invalid OTP! Please try again")
-        return render(request, 'users/otpverification.html')
 
 
 # def otpverification(request):
