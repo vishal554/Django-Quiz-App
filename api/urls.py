7e03e9f8a210e6a20deaad3774253bc7190d1afb
Vishal Panchal <vishalpanchal1338@gmail.com>
2021-07-10 17:03:50 +0530

    Otp Verification

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('home/', home, name='home'),
     path('register/', register, name='register'),
+    path('otpverification/', otpverification, name='otpverification'),
     path('login/', obtain_auth_token, name='login'),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
-from django.http import response
+import random
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from rest_framework.authtoken.models import Token
 from api.serializers import QuizSerializer, UserSerializer
 from users.models import Quiz
-from django.shortcuts import redirect, render
-from rest_framework.decorators import api_view
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import serializers, status
 from rest_framework.response import Response
 # Create your views here.
@@ -27,16 +31,46 @@ def register(request):
     if serializer.is_valid():
         print(serializer)
         # user = serializer.save()
-        # data['response'] = 'Successfully registered'
-        # data['email'] = serializer.email
-        # data['username'] = user.username
+        data['response'] = 'Success'
+        data['email'] = serializer.data['email']
+        data['username'] = serializer.data['username']
+        data['password'] = request.data['password']
         # token = Token.objects.get(user=user).key
         # data['token'] = token
-        data['serializer'] = serializer
-        return redirect('otp_verification')
+        
+        return Response(data)
 
     else:
         data = serializer.errors
     return Response(data)
 
+
+@api_view(['GET',])
+def otpverification(request):
+    data = {}
+    try:
+        email = request.GET['email']
+        password = request.GET['password']
+        username = request.GET['username']
+        otp = request.session.get('otp','')
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    if otp == '':
+        otp = random.randint(11111, 99999)
+        request.session['otp'] = otp
+        send_mail('OTP Verification',
+                    f'Your OTP is: {otp}', 'vishalpanchal338@gmail.com', [email], fail_silently=False)
+        request.session['otp'] = otp
+        request.session['email'] = email
+        request.session['password'] = password
+        request.session['username'] = username 
+        data['response'] = 'Success'
+        return Response(data)
+    else:
+        data['response'] = 'Already sent'
+        return Response(data)
     
+
+
+# @api_view(['POST', ])
+# def verify_otp(request):
