31e9d29049c43902049849ac9f420591f9328c21
Vishal Panchal <vishalpanchal1338@gmail.com>
2021-07-22 08:16:24 +0530

    added security

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import config
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,11 +140,14 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
+# load_dotenv()
+# email = os.getenv('GMAIL_EMAILID')
+# passw = os.getenv('GMAIL_PASSWORD')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
-EMAIL_HOST_USER = config('GMAIL_EMAILID')
+EMAIL_HOST_USER = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465 
 EMAIL_USE_TLS = False 
 EMAIL_USE_SSL = True 
-EMAIL_HOST_PASSWORD = config('GMAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = ''
